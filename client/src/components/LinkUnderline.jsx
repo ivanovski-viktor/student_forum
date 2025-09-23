fbe71103df444ad5a8572c5106519c8c13908849
viksa codes <viktor.ivanovski11@gmail.com>
2025-09-23 23:10:39 +0200

    fontawesome added

--- client/src/components/LinkUnderline.jsx
+++ client/src/components/LinkUnderline.jsx
@@ -1,13 +1,18 @@
 import { Link } from "react-router-dom";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRightLong } from "@fortawesome/free-solid-svg-icons";
 export default function LinkUnderline({ link, text, ...rest }) {
   return (
     <Link
-      className="text-green-600 no-underline underline-offset-2 hover:underline mx-auto"
+      className="text-green-600 no-underline underline-offset-2 hover:underline mx-auto group"
       to={link}
       {...rest}
     >
-      {text}
+      {text}{" "}
+      <FontAwesomeIcon
+        className="opacity-0 max-w-0 w-4 transition-all duration-200 ease-in-out group-hover:max-w-4 group-hover:opacity-100 group-hover:ml-0.5"
+        icon={faArrowRightLong}
+      />
     </Link>
   );
 }
