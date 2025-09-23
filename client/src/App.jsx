aa90db667fb999e9e35c2b2db46cfbe7ec010688
viksa codes <viktor.ivanovski11@gmail.com>
2025-09-23 22:45:36 +0200

    isolate loader code in component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,13 @@
-import { Routes, Route, Link, useLocation } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Register from "./pages/Register.jsx";
 import Login from "./pages/Login.jsx";
 import Loader from "./components/Loader.jsx";
-import { useEffect, useState } from "react";
 
 function App() {
-  const location = useLocation();
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    setLoading(true);
-    const timer = setTimeout(() => setLoading(false), 900);
-
-    return () => clearTimeout(timer);
-  }, [location]);
-
   return (
     <div>
-      {
-        <Loader
-          activeClass={
-            loading ? "opacity-100" : "transition-opacity duration-300 ease-in"
-          }
-        />
-      }
+      <Loader />
 
       <nav className="p-4 border-b border-gray-200 flex gap-4">
         <Link to="/">Home</Link>
