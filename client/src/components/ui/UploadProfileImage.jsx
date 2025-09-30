00da11992d1f10c7f476896badc54b9e938d0af8
viksa codes <viktor.ivanovski11@gmail.com>
2025-09-30 23:13:15 +0200

    users work in progress

---
+++ client/src/components/ui/UploadProfileImage.jsx
@@ -0,0 +1,60 @@
+import { useRef } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPen } from "@fortawesome/free-solid-svg-icons";
+
+const API = import.meta.env.VITE_API_URL;
+
+export default function UploadProfileImage() {
+  const fileInputRef = useRef(null);
+
+  const handleButtonClick = () => {
+    fileInputRef.current.click(); // Trigger file input
+  };
+
+  const handleFileChange = async (event) => {
+    const file = event.target.files[0];
+    if (!file) return;
+
+    const token = localStorage.getItem("token");
+    const formData = new FormData();
+    formData.append("profile_picture", file);
+
+    try {
+      const response = await fetch(`${API}/users/me/profile-picture`, {
+        method: "POST",
+        headers: {
+          Authorization: token,
+        },
+        body: formData,
+      });
+
+      if (!response.ok) {
+        throw new Error(`Upload failed: ${response.status}`);
+      }
+
+      alert("Image uploaded successfully");
+    } catch (error) {
+      alert("Error uploading image:", error);
+    }
+  };
+
+  return (
+    <>
+      <button
+        type="button"
+        onClick={handleButtonClick}
+        className="text-xs absolute top-2 right-2 text-white transition-opacity duration-200 ease-in-out hover:opacity-60 cursor-pointer bg-orange-500 w-6 h-6 rounded-full"
+      >
+        <FontAwesomeIcon icon={faPen} />
+      </button>
+
+      <input
+        type="file"
+        accept="image/*"
+        ref={fileInputRef}
+        style={{ display: "none" }}
+        onChange={handleFileChange}
+      />
+    </>
+  );
+}
--- client/src/pages/MyAccount.jsx
+++ client/src/pages/MyAccount.jsx
@@ -1,6 +1,13 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import InlineLoader from "../components/layout/InlineLoader";
+import UploadProfileImage from "../components/ui/UploadProfileImage";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRightLong } from "@fortawesome/free-solid-svg-icons";
+
+import logout from "../helper-functions/logout";
+
+import userPlaceholder from "../assets/user-placeholder.png";
 
 export default function MyAccount() {
   const navigate = useNavigate();
@@ -55,10 +62,52 @@ export default function MyAccount() {
 
   if (error) return <div>Error: {error}</div>;
 
+  const { profile_image_url, id, username, email, created_at } = userData.user;
+
   return (
-    <div>
-      <h2>My Account</h2>
-      <div className="">{JSON.stringify(userData.user)}</div>
+    <div className="container mx-auto px-6 md:px-8 py-10 md:py-16">
+      <div className=" max-w-2xl mx-auto p-6 sm:p-10 border shadow-2xl shadow-gray-200 border-orange-300 bg-white rounded-xl ">
+        <h2 className="mb-6 text-center text-2xl font-bold ">My Profile</h2>
+        <div className="sm:grid sm:grid-cols-4">
+          <div className="sm:col-span-1 max-sm:w-40 max-sm:mx-auto">
+            {profile_image_url ? (
+              <div className="relative">
+                <UploadProfileImage />
+                <img
+                  className="rounded-xl w-full aspect-[1/1.25] object-cover"
+                  src={profile_image_url}
+                  alt="placeholder image"
+                />
+              </div>
+            ) : (
+              <div className="relative">
+                <UploadProfileImage />
+                <img
+                  className=" pt-10 bg-orange-600 rounded-xl aspect-[1/1.25] object-cover"
+                  src={userPlaceholder}
+                  alt="placeholder image"
+                />
+              </div>
+            )}
+            <img src="" alt="" />
+          </div>
+          <div className="sm:col-span-3"></div>
+        </div>
+        <div className="mt-6 flex items-center justify-center">
+          <button
+            onClick={logout}
+            className="text-orange-600 no-underline underline-offset-2 hover:underline mx-auto cursor-pointer group"
+          >
+            Logout
+            <FontAwesomeIcon
+              className="opacity-0 max-w-0 w-4 transition-all duration-200 ease-in-out group-hover:max-w-3 group-hover:opacity-100 group-hover:ml-1"
+              icon={faArrowRightLong}
+            />
+          </button>
+        </div>
+      </div>
+
+      {/* <div className="">{JSON.stringify(userData.user)}test</div> */}
     </div>
   );
 }
