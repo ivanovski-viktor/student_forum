1d86118cb2c8d130cbf263621416281b1f695d00
viksa codes <viktor.ivanovski11@gmail.com>
2025-10-18 20:27:12 +0200

    fixed cascading deletion issue

--- server/db/db.go
+++ server/db/db.go
@@ -4,47 +4,40 @@ import (
 	"database/sql"
 	"log"
 
-	_ "github.com/mattn/go-sqlite3"
-
 	"github.com/golang-migrate/migrate/v4"
 	_ "github.com/golang-migrate/migrate/v4/database/sqlite3"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 var DB *sql.DB
 
 func InitDB() {
-	var err error
-	DB, err = sql.Open("sqlite3", "student_forum.db")
-	if err != nil {
-		log.Fatalf("Could not connect to DB: %v", err)
-	}
+    var err error
+    DB, err = sql.Open("sqlite3", "file:student_forum.db?_foreign_keys=on")
+    if err != nil {
+        log.Fatalf("Could not connect to DB: %v", err)
+    }
 
-	if err := DB.Ping(); err != nil {
-		log.Fatalf("Could not ping DB: %v", err)
-	}
+    if err := DB.Ping(); err != nil {
+        log.Fatalf("Could not ping DB: %v", err)
+    }
 
-	// Enable foreign keys
-	_, err = DB.Exec("PRAGMA foreign_keys = ON")
-	if err != nil {
-		log.Fatalf("Failed to enable foreign keys: %v", err)
-	}
-
-	runMigrations()
+    runMigrations()
 }
 
 func runMigrations() {
-	migrationPath := "file://db/migrations"
-	dbPath := "sqlite3://student_forum.db"
+    migrationPath := "file://db/migrations"
+    dbPath := "sqlite3://student_forum.db"
 
-	m, err := migrate.New(migrationPath, dbPath)
-	if err != nil {
-		log.Fatalf("Failed to initialize migrations: %v", err)
-	}
+    m, err := migrate.New(migrationPath, dbPath)
+    if err != nil {
+        log.Fatalf("Failed to initialize migrations: %v", err)
+    }
 
-	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
-		log.Fatalf("Failed to run migrations: %v", err)
-	}
+    if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+        log.Fatalf("Failed to run migrations: %v", err)
+    }
 
-	log.Println("Migrations applied successfully")
+    log.Println("Migrations applied successfully")
 }
